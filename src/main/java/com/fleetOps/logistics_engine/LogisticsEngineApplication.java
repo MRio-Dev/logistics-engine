579bdf0ec141e2b52f229f7a7b34b9ccd5eb45b6
Manish Raj <manishraj3322@gmail.com>
2026-03-23 01:35:32 +0530

    init commit

---
+++ src/main/java/com/fleetOps/logistics_engine/LogisticsEngineApplication.java
@@ -0,0 +1,13 @@
+package com.fleetOps.logistics_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fleetOps/logistics_engine/LogisticsEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleetOps.logistics_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
